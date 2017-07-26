9d12caf904915e8bb0e74097e030ce584daf380d
lei2350934xj <510539175@qq.com>
2017-07-26 14:08:58 +0800

    seajs-exports的三种方式

--- seajs/js/calculator.js
+++ seajs/js/calculator.js
@@ -18,13 +18,35 @@ define(function(require,exports,module){
         return parseFloat(a) / parseFloat(b);
       }
     
-      // 暴露模块的公共成员
+      // 方式1 暴露模块的公共成员
       exports.add = add;
       exports.subtract = subtract;
       exports.multiply = multiply;
       exports.divide = divide;    
       /**
        * 或者 把function(require,exports,module){ var obj = {// 属性和方法};} 
-       * 内部写成对象的形式 用 modules.exports = obj; 的形式
+       * 内部写成对象的形式 用 module.exports = obj; 的形式
+       */
+      // 方式2
+      var obj = {};
+      obj.add = add;
+      obj.subtract = subtract;
+      obj.multiply = multiply;
+      obj.divide = divide;
+      module.exports = obj;
+      // 方式3
+      function Person(name,age,sex){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+      }
+      Person.prototype.sayHi = function(){
+        console.log(this.name+"say hi");
+      }
+      module.exports = Person;
+      /**
+       * 使用 seajs.use("./calculator.js",function(Person){//});
+       * var person = new Person("llt",23,"男");
+       * person.sayHi();
        */
 });
